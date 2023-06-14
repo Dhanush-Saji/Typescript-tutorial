3f4d59e0206c04f8b9f46572db6ea4ce4a929538
Dhanush-Saji <dhanushsaji1@gmail.com>
2023-06-14 13:30:22 +0530

    Aliases, Objects, Functions

---
+++ functions.ts
@@ -0,0 +1,24 @@
+//Here I'm passing a string to the function and adding the string with a number. This shouldn't be work, but it will. This is why we need to specify the type of the parameter of the function
+function addTwo(num){
+    return num + 2;
+}
+
+addTwo('4')
+
+function addTwoWithType(num:number){
+    return num + 2;
+}
+
+addTwoWithType(5)
+
+//Multiple parameters in a function
+function signup(name:string, password:string,isAuth:boolean){
+
+}
+signup('Dhanush','password',true)
+
+//Giving default value for the functions
+const login = (name:string,pass:string,isAuth:boolean = true)=>{
+
+}
+login('Dhanush','password')
---
+++ object.ts
@@ -0,0 +1,21 @@
+//This means the function is returning an object.
+function createCourse():{}{
+    return {}
+}
+
+function createUser({ name, isAuth }: { name: string, isAuth: boolean }){
+
+}
+createUser({name:'Dhanush',isAuth:false})
+
+
+function createUserReturn():{name:string,isAuth:boolean}{
+    return{name:'Dhanush',isAuth:false}
+}
+
+//There is one problem with the Typescript, Here I didn't defined the email, but still Typescript allow me to pass that in the function call. That's a problem from the Typescript side.
+function createUserNew({ name, isAuth }: { name: string, isAuth: boolean }){
+    console.log(name, isAuth);
+    }
+    let user = {name:'Dhanush',isAuth:false,email:'dhanush@gmail.com'}
+    createUserNew(user)
