372d36e8f974eedbfaae3767881dab322fb088bd
Dhanush Saji <dhanushsaji1@gmail.com>
2023-06-16 02:16:50 +0530

    updates

---
+++ app/src/App.tsx
@@ -0,0 +1,17 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [first, setfirst] = useState('')
+  const changeSetFirst = ()=>{
+    setfirst('5')
+  }
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
